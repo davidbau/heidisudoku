359ca35f04578c8c7b10035053a84885dac399c9
davidbau <davidbau@9de4e726-e42d-420b-91bd-1075d45dd282>
2011-01-17 16:01:16 +0000

    Version 0.10: fix missing css file

--- trunk/src/build.sh
+++ trunk/src/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-VERSION=`grep version manifest.json | sed 's/^.*\(0\.[0-9]*\)".*$/\1/'`
+VERSION=`grep version manifest.json | sed 's/^.*\.\([0-9]*\.[0-9]*\)".*$/\1/'`
 OUTPUT="sudoku-$VERSION.zip"
 rm -f $OUTPUT
-zip -r $OUTPUT . -i \*.html \*.gif \*.png \*.json \*.js \*.ttf
+zip -r $OUTPUT . -i \*.html \*.gif \*.png \*.json \*.js \*.ttf \*.css
 echo $OUTPUT
