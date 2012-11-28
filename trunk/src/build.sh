9752025f36c24ab2cb9e21005869456607f89bc8
davidbau <davidbau@9de4e726-e42d-420b-91bd-1075d45dd282>
2012-11-28 17:24:28 +0000

    v0.58 Fix prevenDefault bug

--- trunk/src/build.sh
+++ trunk/src/build.sh
@@ -2,6 +2,8 @@
 
 VERSION=`grep version manifest.json | sed 's/^.*\.\([0-9]*\.[0-9]*\)".*$/\1/'`
 OUTPUT="sudoku-$VERSION.zip"
+sed "s/<p id=version>[^<]*<\\/p>/<p id=version>Version $VERSION<\\/p>/" about.html > about.html.new
+mv -f about.html.new about.html
 rm -f $OUTPUT
 zip -r $OUTPUT . -i \*.html \*.gif \*.png \*.json \*.js \*.ttf \*.css
 echo $OUTPUT
